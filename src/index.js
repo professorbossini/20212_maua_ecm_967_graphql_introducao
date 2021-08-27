3bad76c314deea5187fbfaac8b3ced3f4b5b8301
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-26 22:08:36 -0300

    Terminamos o pdf 02.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,55 @@
 import { GraphQLServer } from "graphql-yoga";
 
+
+const livros = [
+    {
+        id: '1',
+        titulo: 'Effective Java',
+        genero: "Técnico",
+        edicao: 3,
+        preco: 39.99
+    },
+    {
+        id: '2',
+        titulo: "Concrete Mathematics",
+        genero: "Técnico",
+        edicao: 1,
+        preco: 89.99
+    }
+];
+
+const usuarios = [{
+    id: '100',
+    nome: 'Jose',
+    livros: [{
+        id: '1',
+        titulo: 'Effective Java',
+        genero: "Técnico",
+        edicao: 3,
+        preco: 39.99
+    },
+    {
+        id: '2',
+        titulo: "Concrete Mathematics",
+        genero: "Técnico",
+        edicao: 1,
+        preco: 89.99
+    }
+    ]
+}, {
+    id: '101',
+    nome: 'Maria',
+    livros: [{
+        id: '5',
+        titulo: 'Programming Challenges',
+        genero: "Técnico",
+        edicao: 1,
+        preco: 39.99
+    }]
+}]
+
+
+
 //schema
 // const typeDefs = `
 //     type Query {
@@ -13,6 +63,12 @@ import { GraphQLServer } from "graphql-yoga";
 //     }
 // `;
 const typeDefs = `
+    type Usuario{
+        id: ID!,
+        nome: String!,
+        idade: Int!,
+        livros: [Livro!]
+    },
     type Livro {
         id: ID!
         titulo: String!
@@ -21,13 +77,38 @@ const typeDefs = `
         preco: Float
     },
     type Query {
+        usuarios: [Usuario!]!
+        livros (precoMaximo: Float!): [Livro!]!
+        adicionar (numeros: [Float!]!): Float!
+        notas: [Int!]!
+        bemVindo (nome: String): String!
         effectiveJava: Livro!
     }
 `
 
 const resolvers = {
     Query: {
-        effectiveJava (){
+        livros(parent, args, ctx, info) {
+            return livros.filter((l) => {
+                return l.preco <= args.precoMaximo
+            });
+        },
+        usuarios() {
+            return usuarios;
+        },
+        adicionar(parent, args, ctx, info) {
+            return args.numeros.length === 0 ? 0 :
+                args.numeros.reduce((ac, atual) => {
+                    return ac + atual;
+                })
+        },
+        notas(parent, args, ctx, info) {
+            return [10, 2, 7, 7, 8]
+        },
+        bemVindo(parent, args, ctx, info) {
+            return `Bem vindo ${args.nome ? args.nome : 'visitante'}`;
+        },
+        effectiveJava() {
             return {
                 id: '123456',
                 titulo: 'Effective Java',
@@ -73,5 +154,5 @@ const server = new GraphQLServer({
 
 //
 
-server.start(() => console.log ("Servidor no ar..."))
+server.start(() => console.log("Servidor no ar..."))
 
